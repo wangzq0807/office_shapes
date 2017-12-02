b7f7d23f588a8f1de2601d503c37e301a8bb5c79
王正强 <zhengqiang@cpu-os.ac.cn>
2017-12-02 18:42:29 +0800

    xml handler

--- xmlparser.py
+++ xmlparser.py
@@ -4,31 +4,48 @@ import xml
 from xml import sax
 from xml.parsers.expat import ParserCreate
 
-class PathHandler(object):
+class XmlHandler(object):
     def __init__(self):
-        self.processing = False
+        pass
+    def startElement(self, name, attrs):
+        pass
+    def endElement(self, name):
+        pass
+    def characters(self, content):
+        pass
+
+class PathMoveToHandler(XmlHandler):
+    def __init__(self):
+        pass
+
+class PathHandler(XmlHandler):
+    def __init__(self):
+        self.cur_handler = None
+
     def startElement(self, name, attrs):
         if name == 'path':
-            self.processing = True
-        elif name == 'moveTo':
             pass
+        elif name == 'moveTo':
+            self.cur_handler = PathMoveToHandler()
+        elif self.cur_handler is not None:
+            self.cur_handler.startElement(name, attrs)
 
     def endElement(self, name):
         if name == 'path':
-            self.processing = False
+            pass
 
     def characters(self, content):
         pass
 
-class GuidListContext(object):
-    def __init__(self, guid_list):
-        self.guid_list = guid_list
+class GuidListHandler(XmlHandler):
+    def __init__(self, guid_dict):
+        self.guid_dict = guid_dict
 
     def startElement(self, name, attrs):
         if name == 'gd':
             name = attrs.get('name')
             fmla = attrs.get('fmla')
-            self.guid_list[name] = fmla
+            self.guid_dict[name] = fmla
 
     def endElement(self, name):
         pass
@@ -36,16 +53,17 @@ class GuidListContext(object):
     def characters(self, content):
         pass
 
-class OneShape():
+class OOXMLShape():
     def __init__(self, name):
-        self.guid_list = {}
-        self.adjguid_list = {}
-        self.cur_context = None
+        self.guid_dict = {}
+        self.adjguid_dict = {}
+        self.path_list = []
+        self.cur_handler = None
     def startElement(self, name, attrs):
         if name == 'avLst':     # adjustment
-            self.cur_context = GuidListContext(self.adjguid_list)
+            self.cur_handler = GuidListHandler(self.adjguid_dict)
         elif name == 'gdLst':   # fmla
-            self.cur_context = GuidListContext(self.guid_list)
+            self.cur_handler = GuidListHandler(self.guid_dict)
         elif name == 'ahLst':   # control point
             pass
         elif name == 'cxnLst':  # vertex
@@ -54,40 +72,44 @@ class OneShape():
             pass
         elif name == 'pathLst': # path
             pass
-        elif self.cur_context is not None:
-            self.cur_context.startElement(name, attrs)
+        elif self.cur_handler is not None:
+            self.cur_handler.startElement(name, attrs)
 
     def endElement(self, name):
         pass
     def characters(self, content):
         pass
 
-class framework(xml.sax.ContentHandler):
+class XmlRootHandler(xml.sax.ContentHandler):
     def __init__(self):
-        super(framework, self).__init__()
+        super(XmlRootHandler, self).__init__()
         self.level = 0
         self.shape_list = {}
-        self.cur_context = None
+        self.cur_handler = None
 
     def startElement(self, name, attrs):
         self.level += 1
         if self.level == 2:
-            shape = OneShape(name)
+            if name == 'quadArrow':
+                print(name)
+            shape = OOXMLShape(name)
             self.shape_list[name] = shape
-            self.cur_context = shape
-        elif self.level > 2 and self.cur_context is not None:
-            self.cur_context.startElement(name, attrs)
+            self.cur_handler = shape
+        elif self.level > 2 and self.cur_handler is not None:
+            self.cur_handler.startElement(name, attrs)
 
     def endElement(self, name):
-        if self.level > 2 and self.cur_context is not None:
-            self.cur_context.endElement(name)
+        if self.level > 2 and self.cur_handler is not None:
+            self.cur_handler.endElement(name)
+        elif self.level == 2 and self.cur_handler is not None:
+            pass
         self.level -= 1
 
     def characters(self, content):
         pass
 
 if __name__ == '__main__':
-    handler = framework()
+    handler = XmlRootHandler()
     parser = xml.sax.make_parser()
     parser.setContentHandler( handler )
     parser.parse('presetShapeDefinitions.xml')
